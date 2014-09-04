645a4e926fadbcf496c5d0b79db3597681320be6
Gabriel Barta <gbarta@gabrielbarta.com>
2014-09-04 19:46:33 +1000

    whitespace: no tabs

--- evil-noautochdir.el
+++ evil-noautochdir.el
@@ -4,15 +4,15 @@
   (let ((default-directory evil-cwd))
     ;; This depends on subtle and surprising behaviour which
     ;; happens to work out exactly as we would like due to
-	;; dynamic scoping and buffer-local variables, but probably
-	;; only due to an implementation detail.
+    ;; dynamic scoping and buffer-local variables, but probably
+    ;; only due to an implementation detail.
     ;; By overriding default-directory with a scoped variable,
     ;; all of the evil and minibuffer and completion code will
-	;; get the directory we have set to emilate noautochdir.
+    ;; get the directory we have set to emilate noautochdir.
     ;; However, if emacs sets the variable e.g. as part of
     ;; find-file, it will change the real buffer-local value
     ;; and not this temporary value, keeping normal emacs
-	;; behaviour of default-directory!
+    ;; behaviour of default-directory!
     ad-do-it))
 
 (evil-define-command evil-cd (count dir)
