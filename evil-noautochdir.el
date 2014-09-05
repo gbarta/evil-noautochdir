5ebfdcd92715f41e18897c76c1e9e653f70ec376
Gabriel Barta <gbarta@gabrielbarta.com>
2014-09-05 22:57:34 +1000

    extract with-evil-cwd macro for external use

--- evil-noautochdir.el
+++ evil-noautochdir.el
@@ -1,19 +1,23 @@
 (defvar evil-cwd default-directory)
 
+(defmacro with-evil-cwd (&rest body)
+  `(let ((default-directory evil-cwd))
+     (progn ,@body)))
+
 (defadvice evil-ex (around evil-ex-noautochdir () activate)
-  (let ((default-directory evil-cwd))
-    ;; This depends on subtle and surprising behaviour which
-    ;; happens to work out exactly as we would like due to
-    ;; dynamic scoping and buffer-local variables, but probably
-    ;; only due to an implementation detail.
-    ;; By overriding default-directory with a scoped variable,
-    ;; all of the evil and minibuffer and completion code will
-    ;; get the directory we have set to emilate noautochdir.
-    ;; However, if emacs sets the variable e.g. as part of
-    ;; find-file, it will change the real buffer-local value
-    ;; and not this temporary value, keeping normal emacs
-    ;; behaviour of default-directory!
-    ad-do-it))
+  (with-evil-cwd
+   ;; This depends on subtle and surprising behaviour which
+   ;; happens to work out exactly as we would like due to
+   ;; dynamic scoping and buffer-local variables, but probably
+   ;; only due to an implementation detail.
+   ;; By overriding default-directory with a scoped variable,
+   ;; all of the evil and minibuffer and completion code will
+   ;; get the directory we have set to emilate noautochdir.
+   ;; However, if emacs sets the variable e.g. as part of
+   ;; find-file, it will change the real buffer-local value
+   ;; and not this temporary value, keeping normal emacs
+   ;; behaviour of default-directory!
+   ad-do-it))
 
 (evil-define-command evil-cd (count dir)
   "Change directory."
