7db409d42a91bbbeee72882f6368313c7843de05
Gabriel Barta <gbarta@gabrielbarta.com>
2017-01-11 18:05:13 +1100

    Avoid more byte-compilation issues in spacemacs

--- evil-noautochdir.el
+++ evil-noautochdir.el
@@ -1,13 +1,13 @@
 ;;; evil-noautochdir.el --- equivalent to set noautochdir for evil
 
-;; Copyright (C) 2014-2016 Gabriel Barta
+;; Copyright (C) 2014-2017 Gabriel Barta
 
 ;; Author: Gabriel Barta <gbarta@gabrielbarta.com>
-;; Package-Requires: ((evil "1.2.3"))
+;; Package-Requires: ((evil "1.0.0"))
 ;; Keywords: evil
-;; Homepage: https://github.com/gbarta/evil-noautochdir
-;; Created: 27th Feb 2015
-;; Version: 0.2.1
+;; URL: https://github.com/gbarta/evil-noautochdir
+;; Version: 0.3.0
+;; Package-Version: 20170111.1800
 
 ;; The MIT License (MIT)
 ;;
@@ -32,13 +32,11 @@
 ;;; Commentary:
 
 ;; This plugin provides an Evil implementation of the default Vim directory
-;; behaviour, "set noautochdir". It only affects Evil ex commands while other
-;; emacs functionality remains unaffected. It provides :pwd and :cd commands
-;; to control the current directory.
+;; behaviour, "set noautochdir". It only affects commands issued from the evil
+;; ex command-line, otherwise emacs functionality remains unaffected. It
+;; provides :pwd and :cd commands to control the current directory.
 
 ;;; Code:
-
-(require 'evil)
 
 (defvar evil-cwd default-directory
   "Current working directory for issuing ex commands.")
@@ -47,39 +45,44 @@
   `(let ((default-directory evil-cwd))
      (progn ,@body)))
 
-(defadvice evil-ex (around evil-ex-noautochdir () activate)
-  (with-evil-cwd
-   ;; This depends on subtle and surprising behaviour which
-   ;; happens to work out exactly as we would like due to
-   ;; dynamic scoping and buffer-local variables, but probably
-   ;; only due to an implementation detail.
-   ;; By overriding default-directory with a scoped variable,
-   ;; all of the evil and minibuffer and completion code will
-   ;; get the directory we have set to emulate noautochdir.
-   ;; However, if emacs sets the variable e.g. as part of
-   ;; find-file, it will change the real buffer-local value
-   ;; and not this temporary value, keeping normal emacs
-   ;; behaviour of default-directory!
-   ad-do-it))
-
-(evil-define-command evil-cd (dir)
+(defun evil-noautochdir-cd (dir)
   "Change directory."
-  (interactive "<f>")
   (when (not dir)
     (setq dir "~"))
   (if (file-directory-p dir)
-      (progn
-        (setq evil-cwd (expand-file-name dir))
-        (message evil-cwd))
+    (progn
+      (setq evil-cwd (expand-file-name dir))
+      (message evil-cwd))
     (error "Can't find directory %s in path" dir)))
-(evil-ex-define-cmd "cd" 'evil-cd)
 
-(defun evil-pwd ()
+(defun evil-noautochdir-pwd ()
   "Print current directory."
   (interactive)
   (message evil-cwd))
-(evil-ex-define-cmd "pwd" 'evil-pwd)
 
+(eval-after-load 'evil
+                 '(eval '(progn
+                           (defadvice evil-ex (around evil-ex-noautochdir () activate)
+                                      (with-evil-cwd
+                                        ;; This depends on subtle and surprising behaviour which
+                                        ;; happens to work out exactly as we would like due to
+                                        ;; dynamic scoping and buffer-local variables, but probably
+                                        ;; only due to an implementation detail.
+                                        ;; By overriding default-directory with a scoped variable,
+                                        ;; all of the evil and minibuffer and completion code will
+                                        ;; get the directory we have set to emulate noautochdir.
+                                        ;; However, if emacs sets the variable e.g. as part of
+                                        ;; find-file, it will change the real buffer-local value
+                                        ;; and not this temporary value, keeping normal emacs
+                                        ;; behaviour of default-directory!
+                                        ad-do-it))
+
+                           (evil-define-command evil-cd (dir)
+                                                :repeat nil
+                                                (interactive "<f>")
+                                                (evil-noautochdir-cd dir))
+                           (evil-ex-define-cmd "cd" 'evil-cd)
+                           (evil-ex-define-cmd "pwd" 'evil-noautochdir-pwd))))
 
 (provide 'evil-noautochdir)
 ;;; evil-noautochdir.el ends here
